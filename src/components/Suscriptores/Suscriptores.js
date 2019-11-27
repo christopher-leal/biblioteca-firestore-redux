a60805c9812aac29991381308f99d25300f0d8b5
Christopher Leal <chris_t_o@hotmail.com>
2019-11-26 23:23:28 -0600

    Se termino la lista de los componentes con la redireccion correcta

--- src/components/Suscriptores/Suscriptores.js
+++ src/components/Suscriptores/Suscriptores.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import Spinner from '../layouts/Spinner';
 
 const Suscriptores = () => {
 	useFirestoreConnect([
@@ -9,14 +11,56 @@ const Suscriptores = () => {
 	const suscriptores = useSelector(
 		(state) => state.firestore.ordered.suscriptores
 	);
+
 	if (!isLoaded(suscriptores)) {
-		return <div>Loading...</div>;
+		return <Spinner />;
 	}
 
 	if (isEmpty(suscriptores)) {
 		return <div>Todos List Is Empty</div>;
 	}
-	return <div>hola</div>;
+	return (
+		<div className="row">
+			<div className="col-md-12 mb-4">
+				<Link to={'/suscriptores/nuevo'} className="btn btn-primary">
+					<i className="la la-plus" /> Nuevo suscriptor
+				</Link>
+			</div>
+			<div className="col-md-8">
+				<h2>
+					<i className="las la-users" />Suscriptores
+				</h2>
+			</div>
+			<table className="table table-striped mt-4">
+				<thead className="text-light bg-primary">
+					<tr>
+						<th>Nombre</th>
+						<th>Carrera</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					{suscriptores.map((suscriptor) => (
+						<tr key={suscriptor.id}>
+							<td>
+								{suscriptor.nombre} {suscriptor.apellido}
+							</td>
+							<td>{suscriptor.carrera}</td>
+							<td>
+								<Link
+									to={`/suscriptor/${suscriptor.id}`}
+									className="btn btn-success btn-block"
+								>
+									<i className="la la-info-circle" /> Mas
+									informacion
+								</Link>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default Suscriptores;
